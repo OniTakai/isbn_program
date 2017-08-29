b8226eec5668b36573234c569e0afb6dcacbbd2c
OniTakai <steven.miller.it@gmail.com>
2017-08-29 15:46:10 -0400

    Got input cleaned up

--- isbn.rb
+++ isbn.rb
@@ -1,6 +1,6 @@
 require_relative "tests.rb"
 
-def isbn_check_11(number)
+def isbn_check_10(number)
     string = number 
 if string.length == 10
     true
@@ -17,4 +17,14 @@ else
     false
 end 
 end 
-isbn_check_11("100")
+isbn_check_10("100")
+isbn_check_13("100")
+
+def clean_up(number)
+    my_string = number
+    my_string.gsub!("-","")
+    my_string.gsub!(" ","")
+    clean_number = my_string
+    puts my_string
+end
+clean_up("1-2-3-444 566")
--- tests.rb
+++ tests.rb
@@ -1,13 +1,23 @@
 require "minitest/autorun"
 require_relative "isbn.rb"
 class TestISBN < Minitest::Test
-    def test_length_11
+    def test_length_10
        isbn = "1234"
-       assert_equal(false, isbn_check_11(isbn))
+       assert_equal(false, isbn_check_10(isbn))
     end
     
-    def test_length_11
+    def test_length_10
         isbn = "1234567890"
-        assert_equal(true, isbn_check_11(isbn))
+        assert_equal(true, isbn_check_10(isbn))
+    end
+
+    def test_length_13
+        isbn = "1234567890123"
+        assert_equal(true, isbn_check_13(isbn))
+    end
+
+    def test_length_13
+        isbn = "120"
+        assert_equal(false, isbn_check_13(isbn))
     end
 end 
