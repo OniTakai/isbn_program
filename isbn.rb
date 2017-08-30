0d27e3dba3f895371e08676a73177f2adcf4c743
OniTakai <steven.miller.it@gmail.com>
2017-08-30 10:23:33 -0400

    Got clean test working

--- isbn.rb
+++ isbn.rb
@@ -1,30 +1,26 @@
-require_relative "tests.rb"
+#def isbn_check_10(number)
+#    string = number 
+#if string.length == 10
+#    true
+#else 
+#    false
+#end
+#end 
 
-def isbn_check_10(number)
-    string = number 
-if string.length == 10
-    true
-else 
-    false
-end
-end 
+#def isbn_check_13(number)
+#string = number 
+#if string.length == 13
+#    true
+#else 
+#    false
+#end 
+#end 
 
-def isbn_check_13(number)
-string = number 
-if string.length == 13
-    true
-else 
-    false
-end 
-end 
-isbn_check_10("100")
-isbn_check_13("100")
 
 def clean_up(number)
-    my_string = number
-    my_string.gsub!("-","")
-    my_string.gsub!(" ","")
-    clean_number = my_string
-    puts my_string
+    number = number.delete("-")
+    number = number.delete(" ")
+    isbn_clean = number
 end
-clean_up("1-2-3-444 566")
+
+
--- tests.rb
+++ tests.rb
@@ -1,23 +1,27 @@
 require "minitest/autorun"
 require_relative "isbn.rb"
 class TestISBN < Minitest::Test
-    def test_length_10
-       isbn = "1234"
-       assert_equal(false, isbn_check_10(isbn))
-    end
+    #def test_length_10
+      # isbn = "1234"
+     #  assert_equal(false, isbn_check_10(isbn))
+    #end
     
-    def test_length_10
-        isbn = "1234567890"
-        assert_equal(true, isbn_check_10(isbn))
-    end
+    #def test_length_10
+      #  isbn = "1234567890"
+     #   assert_equal(true, isbn_check_10(isbn))
+    #end
 
-    def test_length_13
-        isbn = "1234567890123"
-        assert_equal(true, isbn_check_13(isbn))
-    end
+    #def test_length_13
+      #  isbn = "1234567890123"
+     #   assert_equal(true, isbn_check_13(isbn))
+    #end
 
-    def test_length_13
-        isbn = "120"
-        assert_equal(false, isbn_check_13(isbn))
+   # def test_length_13
+  #      isbn = "120"
+ #       assert_equal(false, isbn_check_13(isbn))
+#    end
+
+    def test_clean_num
+        assert_equal("12345",clean_up("12-3 4 - 5"))
     end
 end 
