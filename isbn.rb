ffd643f4959b9a4f7dcc8d9cb334ba287570887a
OniTakai <steven.miller.it@gmail.com>
2017-08-31 16:43:13 -0400

    Got both functions working, now to make sure X works and that my tests pass

--- isbn.rb
+++ isbn.rb
@@ -42,7 +42,6 @@ def check_last_number(isbn_clean)
     my_13_sum = 0
     isbn_clean = isbn_clean.gsub(/\D/, '')
     isbn_clean = isbn_clean
-    puts isbn_clean
     counter = 0
     if isbn_clean.length == 10
     isbn_clean = isbn_clean[0..9]    
@@ -54,7 +53,7 @@ def check_last_number(isbn_clean)
         check_digit = my_sum % 11
         if check_digit.to_s == last_digit.to_s
             my_isbn_array << check_digit.to_s
-            puts "#{my_isbn_array} is a valid ISBN number"
+            puts "#{my_isbn_array} is a valid ISBN 10 number"
         elsif 
         check_digit == 10
             my_isbn_array << "X"
@@ -65,15 +64,27 @@ def check_last_number(isbn_clean)
     elsif isbn_clean.length == 13
         isbn_clean = isbn_clean[0..12]    
         my_isbn_array = isbn_clean.split('')
-        last_digit = my_isbn_array.pop
-        puts my_isbn_array.length
-        while my_isbn_array.length >= counter
-        my_13_sum = my_13_sum + ((my_isbn_array[counter + 1].to_i * 3) + (my_isbn_array[counter].to_i * 1))
-        counter +=1 
-        end  
-    else
+        last_digit_13 = my_isbn_array.pop
+        my_isbn_array.each.with_index do |value,index|
+            if (index + 1) % 2 == 0
+                my_13_sum = my_13_sum + (value.to_i * 3)
+            else
+                my_13_sum = my_13_sum + value.to_i 
+            end  
+        end 
+        check_digit_13 = my_13_sum % 10 
+        check_digit_13_final = (10 - check_digit_13) % 10  
+        if check_digit_13_final.to_s == last_digit_13.to_s
+            my_isbn_array << check_digit_13_final
+            puts "#{my_isbn_array} is a valid ISBN 13 number"
+        elsif check_digit_13_final == 10 
+            my_isbn_array << "X"
+        else 
+            puts "This is not a valid ISBN number"
+        end
+    else     
     puts "This is not a valid ISBN number"
     end 
-puts my_13_sum
 end 
-check_last_number("742x1  3-a-94-761")
+check_last_number("978047-00  59029--")
+
