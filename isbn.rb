cd20588ed17e31b97e27ea20702526ee32c32e9b
OniTakai <steven.miller.it@gmail.com>
2017-08-31 15:07:49 -0400

    Got 10 to work, also filters out all non-digits

--- isbn.rb
+++ isbn.rb
@@ -39,28 +39,41 @@ end
 
 def check_last_number(isbn_clean)
     my_sum = 0
-    isbn_clean = isbn_clean.delete("-")
-    isbn_clean = isbn_clean.delete(" ")
+    my_13_sum = 0
+    isbn_clean = isbn_clean.gsub(/\D/, '')
     isbn_clean = isbn_clean
-    puts isbn_clean.length
+    puts isbn_clean
+    counter = 0
     if isbn_clean.length == 10
-    isbn_clean = isbn_clean[0..8]    
+    isbn_clean = isbn_clean[0..9]    
     my_isbn_array = isbn_clean.split('')
+    last_digit = my_isbn_array.pop
     my_isbn_array.each.with_index do |item,index|
-        my_sum = my_sum + (item.to_f * (index.to_f + 1))
+        my_sum = my_sum + (item.to_i * (index.to_i + 1))
     end 
         check_digit = my_sum % 11
-        if check_digit == 10 
-            my_isbn_array << X
+        if check_digit.to_s == last_digit.to_s
+            my_isbn_array << check_digit.to_s
+            puts "#{my_isbn_array} is a valid ISBN number"
+        elsif 
+        check_digit == 10
+            my_isbn_array << "X"
         else 
-            my_isbn_array << check_digit
+            puts "This is not a valid ISBN"
         end 
-        puts my_sum
-        puts my_isbn_array
-            
+
     elsif isbn_clean.length == 13
+        isbn_clean = isbn_clean[0..12]    
+        my_isbn_array = isbn_clean.split('')
+        last_digit = my_isbn_array.pop
+        puts my_isbn_array.length
+        while my_isbn_array.length >= counter
+        my_13_sum = my_13_sum + ((my_isbn_array[counter + 1].to_i * 3) + (my_isbn_array[counter].to_i * 1))
+        counter +=1 
+        end  
     else
     puts "This is not a valid ISBN number"
     end 
+puts my_13_sum
 end 
-check_last_number("92-----8384 79-9-9")
+check_last_number("742x1  3-a-94-761")
